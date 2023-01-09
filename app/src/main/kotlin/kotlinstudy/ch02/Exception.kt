fefc7e97c9da97c0798b91f49f05b8388d0703ea
small-dogg <sy920112@naver.com>
2023-01-10 02:26:03 +0900

    feat: 2장 코틀린 기초  - try-catch-finally

--- app/src/main/kotlin/kotlinstudy/ch02/Exception.kt
+++ app/src/main/kotlin/kotlinstudy/ch02/Exception.kt
@@ -1,4 +1,44 @@
 package kotlinstudy.ch02
 
+import java.io.BufferedReader
+import java.lang.IllegalArgumentException
+import java.lang.NumberFormatException
+
 class Exception {
+    fun ExptTest(number: Int){
+        val percentage =
+        if(number !in 0..100)
+            number
+        else
+            throw IllegalArgumentException(
+                    "A Percentage value must be between 0 and 100: $number"
+            )
+
+        if (number !in 0..100)
+            throw IllegalArgumentException(
+                    "A Percentage value must be between 0 and 100: $number"
+            )
+    }
+
+    fun readNumber(reader: BufferedReader): Int? {
+        try {
+            val line = reader.readLine()
+            return Integer.parseInt(line)
+        } catch (e: NumberFormatException) {
+            return null
+        } finally {
+            reader.close()
+        }
+    }
+
+    fun readNumber2(reader: BufferedReader) {
+        val number = try {
+            Integer.parseInt(reader.readLine())
+
+        } catch (e: NumberFormatException) {
+            null
+        }
+        println(number)
+    }
+
 }
