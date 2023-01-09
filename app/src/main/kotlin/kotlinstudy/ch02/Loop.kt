84a3ee3309a15352759deed3e9245a2a5cd58f45
small-dogg <sy920112@naver.com>
2023-01-09 23:35:13 +0900

    feat: 2장 코틀린 기초  - map

--- app/src/main/kotlin/kotlinstudy/ch02/Loop.kt
+++ app/src/main/kotlin/kotlinstudy/ch02/Loop.kt
@@ -1,5 +1,9 @@
 package kotlinstudy.ch02
 
+import java.util.*
+
+var binaryReps = TreeMap<Char, String>();
+
 fun fizzBuzz(i: Int) = when{
     i % 15 == 0 -> "FizzBuzz"
     i % 5 == 0 -> "Fizz"
@@ -17,5 +21,20 @@ fun main(){
     println("---------------------")
     for (i in 100 downTo 1 step 2) {
         print (fizzBuzz(i))
+    }
+    println("---------------------")
+
+    for (c in 'A'..'F'){
+        val binary = Integer.toBinaryString(c.code)
+        binaryReps[c] = binary
+    }
+
+    for ((letter, binary) in binaryReps) {
+        println("$letter = $binary")
+    }
+
+    val list = arrayListOf("10", "11","1001")
+    for ((index, element) in list.withIndex()) {
+        println("$index: $element")
     }
 }
