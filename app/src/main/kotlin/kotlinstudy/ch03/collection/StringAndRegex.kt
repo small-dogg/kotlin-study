5817485884a2d9f026884cb960aecfc44595315b
small-dogg <sy920112@naver.com>
2023-01-17 00:08:21 +0900

    feat: 문자열과 정규표현식

--- app/src/main/kotlin/kotlinstudy/ch03/collection/StringAndRegex.kt
+++ app/src/main/kotlin/kotlinstudy/ch03/collection/StringAndRegex.kt
@@ -1,2 +1,36 @@
 package kotlinstudy.ch03.collection
 
+fun main() {
+    println("12.345-6.A".split("\\.|-".toRegex()))
+    println("12.345-6.A".split(".", "-"))
+
+    parsePath("/Users/yole/kotlin-bool/chapter.adoc")
+    parsePathWithRegex("/Users/yole/kotlin-bool/chapter.adoc")
+
+    val kotlinLogo = """|  //
+                       +| //
+                       +|/ \"""
+    println(kotlinLogo.trimMargin("+"))
+}
+
+fun parsePath(path: String) {
+    val directory = path.substringBeforeLast("/")
+    val fullName = path.substringAfterLast("/")
+
+    val filename = fullName.substringBeforeLast(".")
+    val extension = fullName.substringAfterLast(".")
+
+    println("directory = ${directory}")
+    println("fullName = ${fullName}")
+    println("filename = ${filename}")
+    println("extension = ${extension}")
+}
+
+fun parsePathWithRegex(path: String) {
+    val regex = """(.+)/(.+)\.(.+)""".toRegex()
+    val matchResult = regex.matchEntire(path)
+    if (matchResult != null) {
+        val (directory, filename, extension) = matchResult.destructured
+        println("directory = ${directory}, filename = ${filename}, extension = ${extension}")
+    }
+}
